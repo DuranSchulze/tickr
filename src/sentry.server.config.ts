561ecc574b00d25f2c4b05af4ae0d24760e889c6
Zackery Fajardo <zafajardo9.programming@gmail.com>
2026-05-21 14:53:54 +0800

    new updates that are stable

--- src/sentry.server.config.ts
+++ src/sentry.server.config.ts
@@ -1,13 +1,9 @@
 import * as Sentry from '@sentry/tanstackstart-react'
-import { nodeProfilingIntegration } from '@sentry/profiling-node'
 
 if (process.env.VITE_SENTRY_DSN) {
   Sentry.init({
     dsn: process.env.VITE_SENTRY_DSN,
     environment: process.env.NODE_ENV,
-    integrations: [nodeProfilingIntegration()],
     tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.2 : 1.0,
-    profileSessionSampleRate: 1.0,
-    profileLifecycle: 'trace',
   })
 }
