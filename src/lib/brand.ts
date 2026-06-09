5693f28533cd73b7c14b804da34b8836f79e7738
Zackery Fajardo <zafajardo9.programming@gmail.com>
2026-06-09 13:59:49 +0800

    change name

--- src/lib/brand.ts
+++ src/lib/brand.ts
@@ -3,9 +3,9 @@
  * Change these values to update the logo and name everywhere in the app.
  */
 export const BRAND = {
-  name: 'Tickr',
+  name: 'Time Tracker',
   tagline: 'Internal workspace tracking',
   /** Path relative to /public, or an absolute https:// URL */
   logoSrc: '/2.svg',
-  logoAlt: 'Tickr logo',
+  logoAlt: 'Time Tracker logo',
 } as const
